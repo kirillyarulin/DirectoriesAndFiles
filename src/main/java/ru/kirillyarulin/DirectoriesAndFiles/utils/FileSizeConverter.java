07c846b4612e3f0d6c4b9327610dcd671e0fcb27
Kirill Yarulin <kirillyarulin@mail.ru>
2018-07-27 17:58:03 +0300

    added utility class FileSizeConverter with tests

---
+++ src/main/java/ru/kirillyarulin/DirectoriesAndFiles/utils/FileSizeConverter.java
@@ -0,0 +1,33 @@
+package ru.kirillyarulin.DirectoriesAndFiles.utils;
+
+import java.text.DecimalFormat;
+
+/**
+ * Created by Kirill Yarulin on 27.07.18
+ */
+public final class FileSizeConverter {
+    private FileSizeConverter() {
+    }
+
+    public static String format(long sizeInBytes) {
+        DecimalFormat dFormat = new DecimalFormat();
+        dFormat.setDecimalSeparatorAlwaysShown(false);
+
+        if (sizeInBytes < 0) {
+            throw new IllegalArgumentException("Negative file size");
+        }
+
+        if (sizeInBytes < 1024) {
+            return sizeInBytes + "bytes";
+        } else if (sizeInBytes < Math.pow(1024, 2)) {
+            return dFormat.format(sizeInBytes / 1024) + "Kb";
+        } else if (sizeInBytes < Math.pow(1024, 3)) {
+            return dFormat.format(sizeInBytes / Math.pow(1024, 2)) + "Mb";
+        } else if (sizeInBytes < Math.pow(1024, 4)) {
+            return dFormat.format(sizeInBytes / Math.pow(1024, 3)) + "Gb";
+        } else {
+            return dFormat.format(sizeInBytes / Math.pow(1024, 4)) + "Tb";
+        }
+    }
+
+}
---
+++ src/test/java/ru/kirillyarulin/DirectoriesAndFiles/utils/FileSizeConverterTest.java
@@ -0,0 +1,23 @@
+package ru.kirillyarulin.DirectoriesAndFiles.utils;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Kirill Yarulin on 27.07.18
+ */
+public class FileSizeConverterTest {
+
+    @Test
+    public void format() {
+        assertEquals("1.5Gb", FileSizeConverter.format(1610612736));
+        assertEquals("70Mb", FileSizeConverter.format(73400320));
+        assertEquals("3Kb", FileSizeConverter.format(3072));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void formatWithWrongSize() {
+        FileSizeConverter.format(-1);
+    }
+}
